dc228756d483c8497cdb943bee5c51177471822a
Benoit TELLIER <btellier@linagora.com>
2025-02-16 22:43:54 +0100

    Writing test for principals: CardDAV discovery

--- src/test/java/com/linagora/dav/PrincipalTest.java
+++ src/test/java/com/linagora/dav/PrincipalTest.java
@@ -26,6 +26,99 @@
 
 package com.linagora.dav;
 
-public class PrincipalTest {
-    // TODO fill me
+import static com.linagora.dav.DockerOpenPaasExtension.body;
+import static com.linagora.dav.DockerOpenPaasExtension.execute;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.xmlunit.assertj3.XmlAssert;
+
+import io.netty.handler.codec.http.HttpMethod;
+
+class PrincipalTest {
+    @RegisterExtension
+    static DockerOpenPaasExtension dockerOpenPaasExtension = new DockerOpenPaasExtension();
+
+    @Test
+    void currentUserPrincipalShouldLinkTheUserPrincipal() {
+        OpenPaasUser testUser = dockerOpenPaasExtension.newTestUser();
+
+        DockerOpenPaasExtension.Response response = execute(dockerOpenPaasExtension.davHttpClient()
+            .headers(headers -> testUser.basicAuth(headers)
+                .add("Depth", 0)
+                .add("Content-Type", "application/xml"))
+            .request(HttpMethod.valueOf("PROPFIND"))
+            .uri("/")
+            .send(body("""
+                <d:propfind xmlns:d="DAV:">
+                  <d:prop>
+                     <d:current-user-principal />
+                  </d:prop>
+                </d:propfind>""")));
+
+        assertThat(response.status()).isEqualTo(207);
+        XmlAssert.assertThat(response.body())
+            .and("<?xml version=\"1.0\"?>" +
+                "<d:multistatus xmlns:d=\"DAV:\" xmlns:s=\"http://sabredav.org/ns\" xmlns:cal=\"urn:ietf:params:xml:ns:caldav\" xmlns:cs=\"http://calendarserver.org/ns/\" xmlns:card=\"urn:ietf:params:xml:ns:carddav\">" +
+                "<d:response><d:href>/</d:href><d:propstat><d:prop><d:current-user-principal><d:href>/principals/users/" + testUser.id() + "/</d:href></d:current-user-principal></d:prop><d:status>HTTP/1.1 200 OK</d:status></d:propstat></d:response>" +
+                "</d:multistatus>")
+            .ignoreChildNodesOrder()
+            .areSimilar();
+    }
+
+    @Test
+    void shouldShowDisplayName() {
+        OpenPaasUser testUser = dockerOpenPaasExtension.newTestUser();
+
+        DockerOpenPaasExtension.Response response = execute(dockerOpenPaasExtension.davHttpClient()
+            .headers(headers -> testUser.basicAuth(headers)
+                .add("Depth", 0)
+                .add("Content-Type", "application/xml"))
+            .request(HttpMethod.valueOf("PROPFIND"))
+            .uri("/")
+            .send(body("""
+                <d:propfind xmlns:d="DAV:">
+                  <d:prop>
+                     <d:displayname/>
+                     <d:current-user-principal />
+                  </d:prop>
+                </d:propfind>""")));
+
+        assertThat(response.status()).isEqualTo(207);
+        XmlAssert.assertThat(response.body())
+            .and("<?xml version=\"1.0\"?>" +
+                "<d:multistatus xmlns:d=\"DAV:\" xmlns:s=\"http://sabredav.org/ns\" xmlns:cal=\"urn:ietf:params:xml:ns:caldav\" xmlns:cs=\"http://calendarserver.org/ns/\" xmlns:card=\"urn:ietf:params:xml:ns:carddav\">" +
+                "<d:response><d:href>/</d:href><d:propstat><d:prop><d:current-user-principal><d:href>/principals/users/" + testUser.id() + "/</d:href></d:current-user-principal></d:prop><d:status>HTTP/1.1 200 OK</d:status></d:propstat><d:propstat><d:prop><d:displayname/></d:prop><d:status>HTTP/1.1 404 Not Found</d:status></d:propstat></d:response>" +
+                "</d:multistatus>")
+            .ignoreChildNodesOrder()
+            .areSimilar();
+    }
+
+    @Test
+    void shouldAllowAddressBookDiscovery() {
+        OpenPaasUser testUser = dockerOpenPaasExtension.newTestUser();
+
+        DockerOpenPaasExtension.Response response = execute(dockerOpenPaasExtension.davHttpClient()
+            .headers(headers -> testUser.basicAuth(headers)
+                .add("Depth", 0)
+                .add("Content-Type", "application/xml"))
+            .request(HttpMethod.valueOf("PROPFIND"))
+            .uri("/principals/users/" + testUser.id())
+            .send(body("""
+                <d:propfind xmlns:d="DAV:" xmlns:card="urn:ietf:params:xml:ns:carddav">
+                               <d:prop>
+                                  <card:addressbook-home-set />
+                               </d:prop>
+                             </d:propfind>""")));
+
+        assertThat(response.status()).isEqualTo(207);
+        XmlAssert.assertThat(response.body())
+            .and("<?xml version=\"1.0\"?>" +
+                "<d:multistatus xmlns:d=\"DAV:\" xmlns:s=\"http://sabredav.org/ns\" xmlns:cal=\"urn:ietf:params:xml:ns:caldav\" xmlns:cs=\"http://calendarserver.org/ns/\" xmlns:card=\"urn:ietf:params:xml:ns:carddav\">" +
+                "<d:response><d:href>/principals/users/" + testUser.id() + "/</d:href><d:propstat><d:prop><card:addressbook-home-set><d:href>/addressbooks/" + testUser.id() + "/</d:href></card:addressbook-home-set></d:prop><d:status>HTTP/1.1 200 OK</d:status></d:propstat></d:response>" +
+                "</d:multistatus>")
+            .ignoreChildNodesOrder()
+            .areSimilar();
+    }
 }
