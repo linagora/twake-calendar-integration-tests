f0b32d5d3cb3cf0fdfaa2931b49c39f40b8bd80c
vttran <vttran@linagora.com>
2025-09-26 14:41:33 +0700

    CI - pull Tcalendar image first

--- pre-build.sh
+++ pre-build.sh
@@ -2,5 +2,7 @@
 
 docker build -t sabre-v3-it -f Dockerfile.sabre3 .
 docker build -t sabre-v4-it -f Dockerfile.sabre4 .
+
+docker pull linagora/twake-calendar-side-service:branch-master
 docker build -t tcalendar-it -f Dockerfile.tcalendar .
 docker build -t ldap-it -f Dockerfile.ldap .
