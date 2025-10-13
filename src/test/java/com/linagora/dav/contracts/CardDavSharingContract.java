d693cd3a28ad114d744f366ee80540e9a19cf57d
hung phan <hphan@linagora.com>
2025-10-13 16:13:58 +0700

    ISSUE-29 Tests for carddav sharing

---
+++ src/test/java/com/linagora/dav/contracts/CardDavSharingContract.java
@@ -0,0 +1,297 @@
+/********************************************************************
+ *  As a subpart of Twake Mail, this file is edited by Linagora.    *
+ *                                                                  *
+ *  https://twake-mail.com/                                         *
+ *  https://linagora.com                                            *
+ *                                                                  *
+ *  This file is subject to The Affero Gnu Public License           *
+ *  version 3.                                                      *
+ *                                                                  *
+ *  https://www.gnu.org/licenses/agpl-3.0.en.html                   *
+ *                                                                  *
+ *  This program is distributed in the hope that it will be         *
+ *  useful, but WITHOUT ANY WARRANTY; without even the implied      *
+ *  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR         *
+ *  PURPOSE. See the GNU Affero General Public License for          *
+ *  more details.                                                   *
+ ********************************************************************/
+
+package com.linagora.dav.contracts;
+
+import static io.restassured.RestAssured.given;
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.nio.charset.StandardCharsets;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+import com.google.common.collect.ImmutableSet;
+import com.linagora.dav.AddressBookURL;
+import com.linagora.dav.CardDavClient;
+import com.linagora.dav.CardDavClient.PublicRight;
+import com.linagora.dav.DockerTwakeCalendarExtension;
+import com.linagora.dav.DockerTwakeCalendarSetup;
+import com.linagora.dav.OpenPaasUser;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.config.EncoderConfig;
+import io.restassured.config.RestAssuredConfig;
+import io.restassured.http.ContentType;
+
+public abstract class CardDavSharingContract {
+
+    public abstract DockerTwakeCalendarExtension dockerExtension();
+
+    private CardDavClient cardDavClient;
+    private OpenPaasUser alice;
+    private OpenPaasUser bob;
+    private OpenPaasUser cedric;
+
+    @BeforeEach
+    void setUp() {
+        cardDavClient = new CardDavClient(dockerExtension().davHttpClient());
+
+        alice = dockerExtension().newTestUser();
+        bob = dockerExtension().newTestUser();
+        cedric = dockerExtension().newTestUser();
+
+        RestAssured.requestSpecification = new RequestSpecBuilder()
+            .setContentType(ContentType.JSON)
+            .setAccept(ContentType.JSON)
+            .setConfig(RestAssuredConfig.newConfig().encoderConfig(EncoderConfig.encoderConfig().defaultContentCharset(StandardCharsets.UTF_8)))
+            .setBaseUri(dockerExtension().getDockerTwakeCalendarSetupSingleton().getServiceUri(DockerTwakeCalendarSetup.DockerService.SABRE_DAV, "http").toString())
+            .build();
+    }
+
+    @Disabled("https://github.com/linagora/esn-sabre/issues/111")
+    @Test
+    void subscribeShouldThrowErrorWhenAddressBookPublicRightIsHidden() {
+        String addressBook = "collected";
+        cardDavClient.setHiddenPublicRight(bob, bob.id(), addressBook);
+
+        assertThatThrownBy(() -> cardDavClient.subscribe(alice, bob.id(), addressBook, "new book"))
+            .hasMessageContaining("Unexpected status code: 403");
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = PublicRight.class, names = {"READ", "READ_WRITE"})
+    void subscribeShouldSucceedWhenAddressBookPublicRightIsNotHidden(PublicRight right) {
+        String addressBook = "collected";
+        String copiedAddressBook = "new book";
+
+        // GIVEN Bob has a address book named "collected"
+        // AND the public right of this address book is not hidden
+        cardDavClient.setPublicRight(bob, bob.id(), addressBook, right);
+
+        // WHEN Alice subscribes to Bob's "collected" address book
+        cardDavClient.subscribe(alice, bob.id(), addressBook, copiedAddressBook);
+
+        String response = given()
+            .headers("Authorization", alice.impersonatedBasicAuth())
+            .queryParam("contactsCount", true)
+            .queryParam("inviteStatus", 2)
+            .queryParam("personal", true)
+            .queryParam("shared", true)
+            .queryParam("subscribed", true)
+            .when()
+            .get("/addressbooks/" + alice.id() + ".json")
+            .then()
+            .extract()
+            .body()
+            .asString();
+
+        // THEN a copy of Bob's address book is visible in Alice's address book list
+        assertThatJson(response)
+            .inPath("_embedded.dav:addressbook[2]")
+            .isEqualTo("""
+                {
+                    "_links": {
+                        "self": {
+                            "href": "${json-unit.ignore}"
+                        }
+                    },
+                    "dav:name": "{addressBook}",
+                    "carddav:description": "",
+                    "dav:acl": [
+                        "dav:read",
+                        "dav:write"
+                    ],
+                    "dav:share-access": null,
+                    "openpaas:subscription-type": "public",
+                    "type": null,
+                    "state": null,
+                    "numberOfContacts": null,
+                    "acl": [
+                        {
+                            "privilege": "{DAV:}all",
+                            "principal": "principals/users/{aliceId}",
+                            "protected": true
+                        }
+                    ],
+                    "dav:group": null,
+                    "openpaas:source": "/addressbooks/{bobId}/collected.json"
+                }
+                """.replace("{addressBook}", copiedAddressBook)
+                .replace("{aliceId}", alice.id())
+                .replace("{bobId}", bob.id()));
+    }
+
+    @Test
+    void userCannotSeeHiddenAddressBook() {
+        String addressBook = "collected";
+        String copiedAddressBook = "new book";
+        String vcardUid = "test-contact-uid";
+        byte[] vcardPayload = "BEGIN:VCARD\nVERSION:3.0\nFN:John Doe\nEND:VCARD".getBytes(StandardCharsets.UTF_8);
+        cardDavClient.upsertContact(bob, addressBook, vcardUid, vcardPayload);
+
+        cardDavClient.setHiddenPublicRight(bob, bob.id(), addressBook);
+        cardDavClient.subscribe(alice, bob.id(), addressBook, copiedAddressBook);
+
+        assertThatThrownBy(() -> cardDavClient.getContacts(alice, bob.id(), addressBook))
+            .hasMessageContaining("Unexpected status code: 403");
+    }
+
+    @Test
+    void userCanSeePublicAddressBook() {
+        String addressBook = "collected";
+        String vcardUid = "test-contact-uid";
+        byte[] vcardPayload = "BEGIN:VCARD\nVERSION:3.0\nFN:John Doe\nEND:VCARD".getBytes(StandardCharsets.UTF_8);
+        cardDavClient.upsertContact(bob, addressBook, vcardUid, vcardPayload);
+
+        cardDavClient.setPublicRight(bob, bob.id(), addressBook, PublicRight.READ);
+
+        String response = cardDavClient.getContacts(alice, bob.id(), addressBook);
+
+        assertThat(response).contains("John Doe");
+    }
+
+    @Test
+    void createContactDirectlyOnReadOnlyAddressBookShouldThrowError() {
+        String addressBook = "collected";
+
+        cardDavClient.setPublicRight(bob, bob.id(), addressBook, PublicRight.READ);
+
+        String vcardUid = "test-contact-uid";
+        byte[] vcardPayload = "BEGIN:VCARD\nVERSION:3.0\nFN:John Doe\nEND:VCARD".getBytes(StandardCharsets.UTF_8);
+        assertThatThrownBy(() -> cardDavClient.upsertContact(alice, bob.id(), addressBook, vcardUid, vcardPayload))
+            .hasMessageContaining("Unexpected status code: 403");
+    }
+
+    @Test
+    void createContactDirectlyOnHiddenAddressBookShouldThrowError() {
+        String addressBook = "collected";
+
+        cardDavClient.setHiddenPublicRight(bob, bob.id(), addressBook);
+
+        String vcardUid = "test-contact-uid";
+        byte[] vcardPayload = "BEGIN:VCARD\nVERSION:3.0\nFN:John Doe\nEND:VCARD".getBytes(StandardCharsets.UTF_8);
+        assertThatThrownBy(() -> cardDavClient.upsertContact(alice, bob.id(), addressBook, vcardUid, vcardPayload))
+            .hasMessageContaining("Unexpected status code: 403");
+    }
+
+    @Test
+    void createContactDirectlyOnOriginalAddressBookShouldSucceedWhenPublicRightIsReadWrite() {
+        String addressBook = "collected";
+
+        cardDavClient.setPublicRight(bob, bob.id(), addressBook, PublicRight.READ_WRITE);
+
+        String vcardUid = "test-contact-uid";
+        byte[] vcardPayload = "BEGIN:VCARD\nVERSION:3.0\nFN:John Doe\nEND:VCARD".getBytes(StandardCharsets.UTF_8);
+        cardDavClient.upsertContact(alice, bob.id(), addressBook, vcardUid, vcardPayload);
+
+        String response = cardDavClient.getContacts(bob, bob.id(), addressBook);
+
+        assertThat(response).contains("John Doe");
+    }
+
+    @Test
+    void updateContactDirectlyOnOriginalAddressBookShouldSucceedWhenPublicRightIsReadWrite() {
+        String addressBook = "collected";
+        String vcardUid = "test-contact-uid";
+        byte[] vcardPayload = "BEGIN:VCARD\nVERSION:3.0\nFN:John Doe\nEND:VCARD".getBytes(StandardCharsets.UTF_8);
+        cardDavClient.upsertContact(bob, bob.id(), addressBook, vcardUid, vcardPayload);
+
+        cardDavClient.setPublicRight(bob, bob.id(), addressBook, PublicRight.READ_WRITE);
+
+        byte[] vcardPayload2 = "BEGIN:VCARD\nVERSION:3.0\nFN:John Cole\nEND:VCARD".getBytes(StandardCharsets.UTF_8);
+        cardDavClient.upsertContact(alice, bob.id(), addressBook, vcardUid, vcardPayload2);
+
+        String response = cardDavClient.getContacts(bob, bob.id(), addressBook);
+
+        assertThat(response).doesNotContain("John Doe");
+        assertThat(response).contains("John Cole");
+    }
+
+    @Test
+    void deleteContactDirectlyOnOriginalAddressBookShouldSucceedWhenPublicRightIsReadWrite() {
+        String addressBook = "collected";
+        String vcardUid = "test-contact-uid";
+        byte[] vcardPayload = "BEGIN:VCARD\nVERSION:3.0\nFN:John Doe\nEND:VCARD".getBytes(StandardCharsets.UTF_8);
+        cardDavClient.upsertContact(bob, bob.id(), addressBook, vcardUid, vcardPayload);
+
+        cardDavClient.setPublicRight(bob, bob.id(), addressBook, PublicRight.READ_WRITE);
+
+        cardDavClient.deleteContact(alice, bob.id(), addressBook, vcardUid);
+
+        String response = cardDavClient.getContacts(bob, bob.id(), addressBook);
+
+        assertThat(response).doesNotContain("John Doe");
+    }
+
+    @Test
+    void copiedAddressBookShouldNoLongerExistWhenOriginalAddressBookIsHidden() {
+        String addressBook = "collected";
+        String copiedAddressBook = "new book";
+        cardDavClient.setPublicRight(bob, bob.id(), addressBook, PublicRight.READ);
+
+        cardDavClient.subscribe(alice, bob.id(), addressBook, copiedAddressBook);
+
+        cardDavClient.setHiddenPublicRight(bob, bob.id(), addressBook);
+
+        String response = given()
+            .headers("Authorization", alice.impersonatedBasicAuth())
+            .queryParam("contactsCount", true)
+            .queryParam("inviteStatus", 2)
+            .queryParam("personal", true)
+            .queryParam("shared", true)
+            .queryParam("subscribed", true)
+            .when()
+            .get("/addressbooks/" + alice.id() + ".json")
+            .then()
+            .extract()
+            .body()
+            .asString();
+
+        assertThat(response).doesNotContain("\"openpaas:source\":\"\\/addressbooks\\/" + bob.id() + "\\/collected.json\"");
+    }
+
+    @Disabled("https://github.com/linagora/esn-sabre/issues/112 getContacts returns irrelevant data")
+    @Test
+    void copiedAddressBookShouldContainsExistingContactsInOriginalAddressBook() {
+        String addressBook = "collected";
+        String copiedAddressBook = "new book";
+
+        String vcardUid = "test-contact-uid";
+        byte[] vcardPayload = "BEGIN:VCARD\nVERSION:3.0\nFN:John Doe\nEND:VCARD".getBytes(StandardCharsets.UTF_8);
+        cardDavClient.upsertContact(bob, addressBook, vcardUid, vcardPayload);
+
+        cardDavClient.setPublicRight(bob, bob.id(), addressBook, PublicRight.READ);
+        cardDavClient.subscribe(alice, bob.id(), addressBook, copiedAddressBook);
+
+        AddressBookURL addressBookURL = cardDavClient.findUserAddressBooks(alice)
+            .collectList().block().stream()
+            .filter(url -> !ImmutableSet.of("collected", "contacts").contains(url.addressBookId()))
+            .findAny().get();
+
+        String response = cardDavClient.getContacts(alice, alice.id(), addressBookURL.addressBookId());
+
+        assertThat(response).contains("John Doe");
+    }
+}
