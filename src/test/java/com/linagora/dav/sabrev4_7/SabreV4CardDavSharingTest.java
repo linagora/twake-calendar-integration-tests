deba7c40db758ebd792a45b0417b1222fa63c6f0
Benoit TELLIER <btellier@linagora.com>
2025-10-31 09:07:01 +0100

    Run integration tests onto linagora/esn-sabre:sabre-4_7_0-rc1

---
+++ src/test/java/com/linagora/dav/sabrev4_7/SabreV4CardDavSharingTest.java
@@ -0,0 +1,34 @@
+/********************************************************************
+ *  As a subpart of Twake Mail, this file is edited by Linagora.    *
+ *                                                                  *
+ *  https://twake-mail.com/                                         *
+ *  https://linagora.com                                            *
+ *                                                                  *
+ *  This file is subject to The Affero Gnu Public License           *
+ *  version 3.                                                      *
+ *                                                                  *
+ *  https://www.gnu.org/licenses/agpl-3.0.en.html                   *
+ *                                                                  *
+ *  This program is distributed in the hope that it will be         *
+ *  useful, but WITHOUT ANY WARRANTY; without even the implied      *
+ *  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR         *
+ *  PURPOSE. See the GNU Affero General Public License for          *
+ *  more details.                                                   *
+ ********************************************************************/
+
+package com.linagora.dav.sabrev4_7;
+
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import com.linagora.dav.DockerTwakeCalendarExtensionV4_7;
+import com.linagora.dav.contracts.CardDavSharingContract;
+
+public class SabreV4CardDavSharingTest extends CardDavSharingContract {
+    @RegisterExtension
+    static DockerTwakeCalendarExtensionV4_7 dockerExtension = new DockerTwakeCalendarExtensionV4_7();
+
+    @Override
+    public DockerTwakeCalendarExtensionV4_7 dockerExtension() {
+        return dockerExtension;
+    }
+}
