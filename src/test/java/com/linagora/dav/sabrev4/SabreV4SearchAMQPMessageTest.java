d671babf8aeaf783842798cf1239ffd986c4b75b
vttran <vttran@linagora.com>
2025-12-09 12:54:22 +0700

    Adopt Sabre sabre-4_7_0-09-12-2025

--- src/test/java/com/linagora/dav/sabrev4/SabreV4SearchAMQPMessageTest.java
+++ src/test/java/com/linagora/dav/sabrev4/SabreV4SearchAMQPMessageTest.java
@@ -18,6 +18,8 @@
 
 package com.linagora.dav.sabrev4;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
 import com.linagora.dav.DockerTwakeCalendarExtensionV4;
@@ -30,5 +32,19 @@ public class SabreV4SearchAMQPMessageTest extends SearchAMQPMessageContract {
     @Override
     public DockerTwakeCalendarExtensionV4 dockerExtension() {
         return dockerExtension;
+    }
+
+    @Disabled("https://github.com/linagora/esn-sabre/issues/235 Fixed in 4.7.0")
+    @Test
+    @Override
+    protected void shouldReceiveOnlyOneMessageFromEventCancelExchange()  {
+
+    }
+
+    @Disabled("https://github.com/linagora/esn-sabre/issues/235 Fixed in 4.7.0")
+    @Override
+    @Test
+    protected void shouldReceiveOnlyOneMessageFromEventRequestExchange()  {
+
     }
 }
