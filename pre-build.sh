b7140655133bdbb66b3693b3dbca8e8848eea0e5
vttran <vttran@linagora.com>
2025-12-23 11:03:48 +0700

    ci: support custom Sabre 4.7 image in pre-build script

--- pre-build.sh
+++ pre-build.sh
@@ -5,12 +5,14 @@
 if [ -n "$1" ]; then
   echo "Using custom SABRE_4_IMAGE: $1"
   docker build --build-arg SABRE_4_IMAGE="$1" -t sabre-v4-it -f Dockerfile.sabre4 .
+  echo "Using custom SABRE_4_7_IMAGE: $1"
+  docker build --build-arg SABRE_4_7_IMAGE="$1" -t sabre-v4-7-it -f Dockerfile.sabre4_7 .
 else
   echo "Using default SABRE_4_IMAGE from Dockerfile"
   docker build -t sabre-v4-it -f Dockerfile.sabre4 .
+  echo "Using default SABRE_4_7_IMAGE from Dockerfile"
+  docker build -t sabre-v4-7-it -f Dockerfile.sabre4_7 .
 fi
-
-docker build -t sabre-v4-7-it -f Dockerfile.sabre4_7 .
 
 docker pull linagora/twake-calendar-side-service:branch-master
 docker build -t tcalendar-it -f Dockerfile.tcalendar .
