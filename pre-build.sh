4a92fa5717860133276adc660be35447fadb71f0
Benoit TELLIER <btellier@linagora.com>
2026-04-27 14:30:34 +0200

    Force base image pull upon pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -2,13 +2,13 @@
 
 # $1 Sabre v4 docker image to test against. Thought to integrate integration tests directly into Sabre CI.
 
-docker build -t sabre-v4-it -f Dockerfile.sabre4 .
+docker build --pull -t sabre-v4-it -f Dockerfile.sabre4 .
 if [ -n "$1" ]; then
   echo "Using custom SABRE_4_7_IMAGE: $1"
-  docker build --build-arg SABRE_4_7_IMAGE="$1" -t sabre-v4-7-it -f Dockerfile.sabre4_7 .
+  docker build --pull --build-arg SABRE_4_7_IMAGE="$1" -t sabre-v4-7-it -f Dockerfile.sabre4_7 .
 else
   echo "Using default SABRE_4_7_IMAGE from Dockerfile"
-  docker build -t sabre-v4-7-it -f Dockerfile.sabre4_7 .
+  docker build --pull -t sabre-v4-7-it -f Dockerfile.sabre4_7 .
 fi
 
 docker pull linagora/twake-calendar-side-service:branch-master
